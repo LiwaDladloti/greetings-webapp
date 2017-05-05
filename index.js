9f37d42403b4e962776e1e6737ec6df7e6efe52c
LiwaDladloti <liwadladloti@gmail.com>
2017-05-05 15:31:16 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var greetedNames = [];
+
+app.listen(3000, function () {
+	console.log('Server running on port 3000');
+});
+
+app.get('/greetings/:name', function(req, res){
+	console.log(req.params.name);
+	res.send('Hello, ' + req.params.name);
+	greetedNames.push(req.params.name);
+});
+app.get('/greeted', function(req, res){
+	console.log('Showing greeted Names');
+	res.send('These are greeted names: ' + greetedNames);
+});
