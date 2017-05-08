e3337a56e1db15ffdcd0412b4cc0c125e8f26fb8
LiwaDladloti <liwadladloti@gmail.com>
2017-05-08 17:01:03 +0200

    added counter

--- index.js
+++ index.js
@@ -8,13 +8,23 @@ app.listen(3000, function () {
 });
 
 app.get('/greetings/:name', function(req, res){
-	console.log(req.params.name);
-	res.send('Hello, ' + req.params.name);
-	greetedNames.push(req.params.name);
+	var name = req.params.name
+	console.log(name);
+	res.send('Hello, ' + name);
+	greetedNames.push(name);
 });
+
 app.get('/greeted', function(req, res){
 	console.log('Showing greeted Names');
 	res.send('These are greeted names: ' + greetedNames);
 });
-
-
+app.get('/counter/:user', function (req, res) {
+	var user = req.params.user
+	var uniqNames = {};
+	for(var i = 0; i < greetedNames.length; i++) {
+		var greets = greetedNames[i];
+		uniqNames[greets] = uniqNames[greets] ? uniqNames[greets] + 1 : 1;
+	}
+console.log(uniqNames[user]);
+res.send('Hello, ' + user + ' has been greeted ' + JSON.stringify(uniqNames[user]) + ' time(s)');
+});
