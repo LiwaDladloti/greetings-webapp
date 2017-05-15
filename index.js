5a64049b05677d5b07e64f3ccf6500e3aa346ed4
LiwaDladloti <liwadladloti@gmail.com>
2017-05-15 12:39:46 +0200

    changed from using html to using handlebars template

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var body_parser = require('body-parser');
 var app = express();
 app.use(express.static('public'));
-app.use(body_parser.urlencoded());
+
+app.use(body_parser.urlencoded({ extended: false}))
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 var greetedNames = [];
+
+
+app.get('/', function (req, res) {
+	res.render('index');
+});
+
+app.post('/', function(req, res){
+	var name = req.body.name
+//	console.log(name);
+	greetedNames.push(name);
+	var language = req.body.language;
+//	console.log(req.body)
+
+	if (language === 'English') {
+		res.render('index', {msg: 'Hello, ', name: name});
+	} else if (language === 'isiXhosa') {
+		res.render('index', {msg: 'Molo, ',  name: name});
+	} else if (language === 'Latin') {
+		res.render('index', {msg: 'Salve, ', name: name});
+	}
+
+});
 
 app.listen(3000, function () {
 	console.log('Server running on port 3000');
 });
 
-app.post('/greeting', function(req, res){
-	var name = req.body.name
-	console.log(name);
-	res.send('Hello, ' + name);
-	greetedNames.push(name);
-});
-
-app.get('/greeted', function(req, res){
-	console.log('Showing greeted Names');
-	res.send('These are greeted names: ' + greetedNames);
-});
-app.get('/counter/:user', function (req, res) {
-	var user = req.params.user
-	var uniqNames = {};
-	for(var i = 0; i < greetedNames.length; i++) {
-		var greets = greetedNames[i];
-		uniqNames[greets] = uniqNames[greets] ? uniqNames[greets] + 1 : 1;
-	}
-console.log(uniqNames[user]);
-res.send('Hello, ' + user + ' has been greeted ' + JSON.stringify(uniqNames[user]) + ' time(s)');
-});
+//app.get('/greeted', function(req, res){
+//	console.log('Showing greeted Names');
+//	res.send('These are greeted names: ' + greetedNames);
+//});
+//
+//app.get('/counter/:user', function (req, res) {
+//	var user = req.params.user
+//	var uniqNames = {};
+//	for(var i = 0; i < greetedNames.length; i++) {
+//		var greets = greetedNames[i];
+//		uniqNames[greets] = uniqNames[greets] ? uniqNames[greets] + 1 : 1;
+//	}
+//console.log(uniqNames[user]);
+//res.send('Hello, ' + user + ' has been greeted ' + JSON.stringify(uniqNames[user]) + ' time(s)');
+//});
